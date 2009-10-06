85239febc3fb3cc3c14926f39e80c1da3dc02f53
lericson <ludvig@lericson.se>
2009-10-06 22:20:57 +0200

    Add a readme.

---
+++ README.rst
@@ -0,0 +1,5 @@
+This module allows you to set the process title of a process under at least
+Linux and Mac OS X. Usage::
+
+    >>> import procname
+    >>> procname.setprocname("hello world")
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from distutils.core import setup, Extension
 
+readme_conts = open("README.rst", "U").read()
+
 procname_ext = Extension("procname", ["procnamemodule.c"])
 setup(name="procname", version="0.3",
       url="http://github.com/lericson/procname/",
       author="Ludvig Ericson", author_email="ludvig@lericson.se",
       description="Set process titles in Python programs",
+      long_description=readme_conts,
       ext_modules=[procname_ext])
