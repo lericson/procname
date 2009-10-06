d62a98514e6055b7717f477ca2c757d32a810315
lericson <ludvig@lericson.se>
2009-10-06 22:18:20 +0200

    Prepare for PyPI.

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup, Extension
 
 procname_ext = Extension("procname", ["procnamemodule.c"])
-setup(name="procname", version="0.3", author="Ludvig Ericson",
+setup(name="procname", version="0.3",
+      url="http://github.com/lericson/procname/",
+      author="Ludvig Ericson", author_email="ludvig@lericson.se",
       description="Set process titles in Python programs",
       ext_modules=[procname_ext])
